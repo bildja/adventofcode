202d38df1db80bad21e4dbd0ae3a3573c55dfb9d
Dima Bildin <bildja2@gmail.com>
2023-12-22 01:06:07 +0200

    day21

--- 2023/day21.ts
+++ 2023/day21.ts
@@ -1,8 +1,6 @@
 import { day21input } from "./day21input";
-import { createMatr } from "../utils/createMatr";
-import { Coord, cEq } from "../utils/Coord";
+import { Coord } from "../utils/Coord";
 import { getAllNeigbhours } from "../utils/allNeighbours";
-import { fitsTheMatr } from "../utils/fitsTheMatr";
 
 const smallRawInput = `
 ...........
@@ -35,73 +33,107 @@ const findAndReplaceStart = (map: string[][]): Coord => {
   throw Error("no start");
 };
 
-const getPrintedMap = (
-  map: string[][],
-  visitedMap: boolean[][],
-  currentOs: Coord[] = []
-) => {
-  const printedMap = createMatr(map.length, map[0].length, ".");
-  for (let i = 0; i < printedMap.length; i++) {
-    for (let j = 0; j < printedMap[i].length; j++) {
-      printedMap[i][j] =
-        visitedMap[i][j] || currentOs.filter((c) => cEq(c, [i, j])).length
-          ? "O"
-          : map[i][j];
-    }
-  }
-  return printedMap.map((line) => line.join("")).join("\n");
-};
-
 const day21p1 = (rawInput: string, steps: number) => {
   const map = parse(rawInput);
+  const n = map.length;
+  const m = map[0].length;
   const start = findAndReplaceStart(map);
-  const visitedMap = createMatr(map.length, map[0].length, false);
-
-  const printMap = (currentOs: Coord[]) => {
-    console.log(getPrintedMap(map, visitedMap, currentOs), "\n");
-  };
-  let currentOs: Coord[] = [start];
-  const getByCoord = ([i, j]: Coord) => {
-    i %= map.length;
-    j %= map[i].length;
+  const getMapPoint = ([i, j]: Coord): string => {
+    i = ((i % n) + n) % n;
+    j = ((j % m) + m) % m;
     return map[i][j];
   };
 
-  for (let k = 0; k < steps; k++) {
-    // console.log("step", k);
-    // if (k % 2 === 0) {
-    //   printMap(currentOs);
-    // }
-    const newCurrentOs: Coord[] = [];
-    while (currentOs.length) {
-      const [i, j] = currentOs.shift()!;
-      if (k % 2 === 0 && k !== 0) {
-        if (visitedMap[i][j]) {
-          continue;
-        }
-        visitedMap[i][j] = true;
-      }
-      newCurrentOs.push(
-        ...getAllNeigbhours([i, j])
-          // .filter((coord) => fitsTheMatr(coord, map))
-          .filter(([i1, j1]) => map[i1][j1] !== "#")
-          .filter(([i1, j1]) => !visitedMap[i1][j1])
-      );
-    }
-    currentOs = [...newCurrentOs];
-  }
-  while (currentOs.length) {
-    const [i, j] = currentOs.shift()!;
-    visitedMap[i][j] = true;
-  }
+  const key = ([i, j]: Coord) => `${i},${j}`;
+  let visitedMap = new Map<string, Coord>();
+  visitedMap.set(key(start), start);
 
-  return visitedMap
-    .map((line) => line.filter((a) => a).length)
-    .reduce((a, b) => a + b, 0);
+  for (let k = 0; k < steps; k++) {
+    const newVisitedMap = new Map<string, Coord>();
+    for (const coord of visitedMap.values()) {
+      for (const neigbhour of getAllNeigbhours(coord).filter(
+        (coord) => getMapPoint(coord) !== "#"
+      )) {
+        newVisitedMap.set(key(neigbhour), neigbhour);
+      }
+    }
+    visitedMap = newVisitedMap;
+  }
+  return visitedMap.size;
+};
+
+const day21p2 = (rawInput: string, steps: number) => {
+  // no runtime solution so far :(
+  const map = parse(rawInput);
+  const n = map.length;
+  // const a = 15286 / 17161;
+  // const b = 210 / 131;
+  // const c = 1;
+  const a = 15286;
+  const b = 15394;
+  const c = 3884;
+  const x = Math.floor(steps / n);
+  return a * x * x + b * x + c;
 };
 
 console.log(day21p1(smallRawInput, 6));
 console.log(day21p1(day21input, 64));
 
 console.log("\n============== P2 ============\n");
-console.log(day21p1(smallRawInput, 10));
+// console.log(day21p1(day21input, 26501365));
+console.log(day21p2(day21input, 26501365));
+// console.log(day21p2(smallRawInput, 10));
+// console.log(day21p2(smallRawInput, 50)); // 1594
+// console.log(`%c ${day21p2(smallRawInput, 100)}`, "color: red"); // 6536
+for (let i = 2; i < 10; i += 2) {
+  // console.log(`%c ${day21p2(smallRawInput, 131 * i)}`, "color: red"); // 6536
+  // console.log(`%c ${day21p1(day21input, 131 * i)}`, "color: red"); // 6536
+}
+// console.log(`%c ${day21p2(smallRawInput, 121)}`, "color: red"); // 6536
+// console.log(`%c ${day21p2(smallRawInput, 1331)}`, "color: red"); // 6536
+// console.log(`%c ${day21p2(smallRawInput, 100)}`, "color: red"); // 6536
+// console.log(day21p2(smallRawInput, 500)); // 167004
+// console.log(day21p2(smallRawInput, 1000));
+// console.log(day21p2(smallRawInput, 5000));
+// const a: any = [];
+// a.reduce((acc, el, i) => {
+//   if (i % 2) {
+//     acc[i - 1].push(parseInt(el));
+//   } else {
+//     acc.push([parseInt(el.replace("steps").trim())]);
+//   }
+//   return acc;
+// }, []);
+
+const aa = [
+  [131, 16148],
+  [262, 61565],
+  [393, 140174],
+  [524, 245417],
+  [655, 386488],
+  [786, 551557],
+  [917, 755090],
+];
+
+// for (const [steps, expected] of aa) {
+//   const res = day21p2(day21input, steps);
+//   console.log(res, res === expected);
+// }
+
+// x * (524 * 524) + 524 * y + z = 245417
+// x * (655 * 655) + 655 * y + z = 386488
+// x * (786 * 786) + 786 * y + z = 551557
+
+// a = 11999/17161
+// b = 33080/131
+// c = -78887
+
+// a = 16596/17161
+// b = -4371/131
+// c = 3923
+
+// 679198560334555.2 too high
+// 646950270486014
+// 644698974530376 too high
+// 625587097150084
+// 625587094178884 too low
